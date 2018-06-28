01a9ebc8fda7067d5565e3f2c99deb6636d666cc
Abhishek Paradkar <abhishek@Abhisheks-MacBook-Air.local>
2018-06-28 23:12:29 +0530

    leave conversation

---
+++ server/utils/users.js
@@ -0,0 +1,37 @@
+[{
+  id: 'ufufiu6r6ersrt7c',
+  name: 'abhishek',
+  room: 'the office fans'
+}]
+
+class Users {
+  constructor() {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+
+  removeUser (id) {
+    //return the removed user
+    var user = this.getUser(id);
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+    return user;
+  }
+
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+    return namesArray;
+  }
+}
+
+module.exports = {Users};
