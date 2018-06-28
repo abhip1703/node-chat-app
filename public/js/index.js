787218b3a667f7d24ae134f6ad6f0a832757b037
Abhishek Paradkar <abhishek@Abhisheks-MacBook-Air.local>
2018-06-28 14:47:39 +0530

    emit message

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,7 @@
 var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
-  socket.emit('createMessage', {
-    from: 'jen@example.com',
-    text: 'Hi this is me'
-  });
+
 
 });
 socket.on('disconnect', function () {
